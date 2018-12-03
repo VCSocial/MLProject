93906733286777ec6eee13021bdfc7762cb35816
Li Li <ll1@fiu.edu>
2018-12-02 21:24:27 -0500

    Implemented Sarsa as our reinforcement learning algorithm.

---
+++ Simulator/LearningAlgorithms/td_control.py
@@ -0,0 +1,65 @@
+import numpy as np
+import itertools
+from collections import defaultdict
+import sys
+from lib import plotting
+
+
+def make_epsilon_greedy_policy(Q, epsilon, nA):
+    def policy_fn(observation):
+        A = np.ones(nA, dtype=float) * epsilon / nA
+        best_action = np.argmax(Q[observation])
+        A[best_action] += (1.0 - epsilon)
+        return A
+
+    return policy_fn
+
+
+def sarsa(env, num_episodes, discount_factor=1.0, alpha=0.5, epsilon=0.1, num_actions=8):
+
+    Q = defaultdict(lambda: np.zeros(num_actions))
+
+    stats = plotting.EpisodeStats(
+        episode_lengths=np.zeros(num_episodes),
+        episode_rewards=np.zeros(num_episodes))
+
+    policy = make_epsilon_greedy_policy(Q, epsilon, num_actions)
+
+    for i_episode in range(num_episodes):
+
+        # Print out which episode we're on, useful for debugging.
+        if (i_episode + 1) % 100 == 0:
+            print("\rEpisode {}/{}.".format(i_episode + 1, num_episodes), end="")
+            sys.stdout.flush()
+
+        # Reset the environment and pick the first action
+        state = env.reset()
+        action_probs = policy(state)
+        action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
+
+        # One step in the environment
+        for t in itertools.count():
+            # Take a step
+            # Given an action, get the next state, the reward for this action and is this action the last action
+            next_state, reward, done = env.step(action)
+
+            # Pick the next action
+            next_action_probs = policy(next_state)
+            next_action = np.random.choice(np.arange(len(next_action_probs)), p=next_action_probs)
+
+            # Update statistics
+            stats.episode_rewards[i_episode] += reward
+            stats.episode_lengths[i_episode] = t
+
+            # TD Update
+            td_target = reward + discount_factor * Q[next_state][next_action]
+            td_delta = td_target - Q[state][action]
+            Q[state][action] += alpha * td_delta
+
+            if done:
+                break
+
+            action = next_action
+            state = next_state
+
+    return Q, stats
